6f3ebce39648b46619d9032f14b32ac9d9f5c066
yoshinorihanada <yoshihana747@gmail.com>
2021-12-05 23:10:43 +0900

    changed the bootsrtap file for deployment to heroku

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,8 +25,8 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: "7839f9f0265a9f1b4754",
+    cluster: "ap3",
     forceTLS: true
 
     //keyとclusterをdeploy時に変更
