83aecc5048ed25b13fcb0c41a502afe584ba6bad
yoshinorihanada <yoshihana747@gmail.com>
2022-02-15 18:11:40 +0900

    edited pusher enverionment app key on heroku

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,7 +25,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    secret: process.env.PUSHER_SECRET,
+    secret: process.env.PUSHER_APP_SECRET,
     key: process.env.PUSHER_APP_KEY,
     cluster: process.env.PUSHER_APP_CLUSTER,
     forceTLS: true
